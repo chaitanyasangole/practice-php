67aca7b70b1471da82a9900bf093a07646a7f323
chaitany sangole <csjob1994@gmail.com>
2021-01-10 18:30:45 +0530

    array functions shift ,unshift , push,pop files are changed by chaitany on 10/01/2021

--- array_functions_pop_push_demo.php
+++ array_functions_pop_push_demo.php
@@ -2,6 +2,8 @@
 /*
 array_pop(arrayname) :- it removes the last element of array.
 array_push(arrayname,value) :- it adds element to last.
+
+Q. How to add associative array in array_push method ?
 */
 $arr1=array("name"=>'chaitany','dob'=>'1994-10-02','pincode'=>431122,'status'=>true);
 $arr2=array('chaitany','2020-10-02',true,'sangole');
@@ -17,7 +19,7 @@ array_pop($arr2);
 printA($arr1,$arr2);
 
 //array_push_assoc($arr1,'status',true);
-$new=('status'=>false);
+$new=array('status'=>false);
 array_push($arr1,$new);
 array_push($arr2,"Dolas");
 
--- array_functions_shift_unshift_demo.php
+++ array_functions_shift_unshift_demo.php
@@ -1,6 +1,41 @@
 <?php 
+/*
 
+array_shift(arrayname) :- it removes the first element of array.
+array_unshift(arrayname,value) :- it adds element to first.
+
+Q. How to add associative array in array_unshift method ?
+*/
 
 $arr1=array("name"=>'chaitany','dob'=>'1994-10-02','pincode'=>431122,'status'=>true);
 $arr2=array('chaitany','2020-10-02',true,'sangole');
+echo "before call<br>";
+
+printA($arr1,$arr2);
+
+echo "after call<br>";
+array_shift($arr1);
+array_shift($arr2);
+
+printA($arr1,$arr2);
+
+//array_unshift_assoc($arr1,'status',true);
+$new=array('status'=>true);
+array_unshift($arr1,$new);
+array_unshift($arr2,"Dolas");
+
+echo "after call<br>";
+printA($arr1,$arr2);
+
+function printA($arr1,$arr2)
+{
+	echo '<pre>';
+	print_r($arr1);
+	echo '</pre>';
+
+	echo '<pre>';
+	print_r($arr2);
+	echo '</pre>';
+}
+
 ?>
